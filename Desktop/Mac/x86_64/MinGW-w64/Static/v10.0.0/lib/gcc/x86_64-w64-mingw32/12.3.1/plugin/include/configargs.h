b4eac3c72e34760db127793398f20e71911c33e7
Jonathan Gomez <jonnygomez156@gmail.com>
2023-06-29 23:10:04 -0700

    moved alot of libs

--- Desktop/Mac/x86_64/MinGW-w64/Static/v10.0.0/lib/gcc/x86_64-w64-mingw32/12.3.1/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "/Users/jonathangomez/.zeranoe/mingw-w64/src/gcc/configure --target=x86_64-w64-mingw32 --disable-shared --enable-static --disable-multilib --prefix=/Users/jonathangomez/.zeranoe/mingw-w64/x86_64 --enable-languages=c,c++ --disable-nls --enable-threads=posix";
-static const char thread_model[] = "posix";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
--- Desktop/Mac/x86_64/libVLC/4.0.0-dev/Static/share/doc/vlc/libvlc/QtGL/QtGl.pro
+++
@@ -1,9 +0,0 @@
-TEMPLATE = app
-TARGET = qtglvlc
-
-CONFIG += c++14 link_pkgconfig force_debug_info
-PKGCONFIG = libvlc
-QT += widgets
-
-SOURCES += main.cpp qtvlcwidget.cpp
-HEADERS += qtvlcwidget.h
